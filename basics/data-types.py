85b44a654f84216ad029fb0dfe627fb3b95ddffa
mahfuzhasanreza <mahfuzhasanreza1@gmail.com>
2025-11-05 20:06:31 +0600

    modify variables and added data types examples in python basics

---
+++ basics/data-types.py
@@ -0,0 +1,5 @@
+my_number = 10
+my_text = "Hello"
+my_boolean = False
+
+print(type(my_number), type(my_text), type(my_boolean))
--- basics/variables.py
+++ basics/variables.py
@@ -3,4 +3,6 @@ age = 22               # integer
 height = 5.9           # float
 is_student = True      # boolean
 
+print(name, age, height, is_student)
+
 print(f"My name is {name}. I am {age} years old.")
